4b74d399c2d1b302d141933ed387eef610b1d807
unknown <lindsay.kelly15@gmail.com>
2022-03-10 18:37:40 -0500

    adjusted .env

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -2,7 +2,7 @@ function logger(req, res, next) {
     const timestamp = new Date().toLocaleString();
     const method = req.method;
     const url = req.originalUrl;
-    console.log(`${timestamp} ${method} to ${url}`)
+    console.log(`[${timestamp}] ${method} to ${url}`)
     next();
 }
 
